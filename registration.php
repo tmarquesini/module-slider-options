fdc02fdb2623915b018b84d3e1acbfb265229263
Thiago Marquesini <contato@marquesini.com.br>
2023-05-29 07:01:37 -0300

    Initial commit

---
+++ registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'VisualSoftware_SliderOptions', __DIR__);
---
+++ view/base/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_PageBuilder/js/content-type/slider/appearance/default/widget': {
+                'VisualSoftware_SliderOptions/js/content-type/slider/appearance/default/widget-mixin': true
+            }
+        }
+    }
+};
